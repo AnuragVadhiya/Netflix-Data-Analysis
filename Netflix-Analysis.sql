86805238a818e82eef504e57b98c2e8f85f916d7
AnuragVadhiya <anuragvadhiya979@gmail.com>
2025-06-16 13:20:45 +0530

    Add files via upload

---
+++ Netflix-Analysis.sql
@@ -0,0 +1,143 @@
+-- Netflix Content Analysis with SQL
+
+-- Table Creation
+
+DROP TABLE IF EXISTS netflix;
+CREATE TABLE netflix
+(
+    show_id      VARCHAR(5),
+    type         VARCHAR(10),
+    title        VARCHAR(250),
+    director     VARCHAR(550),
+    casts        VARCHAR(1050),
+    country      VARCHAR(550),
+    date_added   VARCHAR(55),
+    release_year INT,
+    rating       VARCHAR(15),
+    duration     VARCHAR(15),
+    listed_in    VARCHAR(250),
+    description  VARCHAR(550)
+);
+
+-- Analysis and Queries
+
+
+-- Q.1: Count the Number of Movies vs TV Shows
+
+SELECT 
+    type,
+    COUNT(*)
+FROM netflix
+GROUP BY 1;
+
+-- Q.2: Find the Most Common Rating for Movies and TV Shows
+
+WITH RatingCounts AS (
+    SELECT 
+        type,
+        rating,
+        COUNT(*) AS rating_count
+    FROM netflix
+    GROUP BY type, rating
+),
+RankedRatings AS (
+    SELECT 
+        type,
+        rating,
+        rating_count,
+        RANK() OVER (PARTITION BY type ORDER BY rating_count DESC) AS rank
+    FROM RatingCounts
+)
+SELECT 
+    type,
+    rating AS most_frequent_rating
+FROM RankedRatings
+WHERE rank = 1;
+
+-- Q.3: List All Movies Released in a Specific Year (e.g., 2020)
+
+SELECT * 
+FROM netflix
+WHERE release_year = 2020;
+
+-- Q.4: Find the Top 5 Countries with the Most Content on Netflix
+
+SELECT * 
+FROM
+(
+    SELECT 
+        UNNEST(STRING_TO_ARRAY(country, ',')) AS country,
+        COUNT(*) AS total_content
+    FROM netflix
+    GROUP BY 1
+) AS t1
+WHERE country IS NOT NULL
+ORDER BY total_content DESC
+LIMIT 5;
+
+-- Q.5: Identify the Longest Movie
+
+SELECT 
+    *
+FROM netflix
+WHERE type = 'Movie'
+ORDER BY SPLIT_PART(duration, ' ', 1)::INT DESC;
+
+-- Q.6: Find Content Added in the Last 5 Years
+
+SELECT *
+FROM netflix
+WHERE TO_DATE(date_added, 'Month DD, YYYY') >= CURRENT_DATE - INTERVAL '5 years';
+
+-- Q.7: Find All Movies/TV Shows by specific Director 
+
+SELECT *
+FROM (
+    SELECT 
+        *,
+        UNNEST(STRING_TO_ARRAY(director, ',')) AS director_name
+    FROM netflix
+) AS t
+WHERE director_name = 'Suhas Kadav';
+
+-- Q.8: List All TV Shows with More Than 3 Seasons
+
+SELECT *
+FROM netflix
+WHERE type = 'TV Show'
+  AND SPLIT_PART(duration, ' ', 1)::INT > 3;
+
+-- Q.9: List All Movies that are Documentaries
+
+SELECT * 
+FROM netflix
+WHERE listed_in LIKE '%Documentaries';
+
+-- Q.10: Find All Content Without a Director
+
+SELECT * 
+FROM netflix
+WHERE director IS NULL;
+
+-- Q.11: Find Movies Featuring '---------'
+
+SELECT * 
+FROM netflix
+WHERE casts LIKE '%Aamir Khan%';
+
+-- Q.12: Categorize Content Based on 'Kill' and 'Violence' Keywords
+
+SELECT 
+    category,
+    COUNT(*) AS content_count
+FROM (
+    SELECT 
+        CASE 
+            WHEN description ILIKE '%kill%' OR description ILIKE '%violence%' THEN 'A/R rated'
+            ELSE 'U/A Rated'
+        END AS category
+    FROM netflix
+) AS categorized_content
+GROUP BY category;
+
+-- End 
